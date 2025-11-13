644020f717926093a4ed65ea9618f06a8a649349
Teinble <xilingzhao16@gmail.com>
2025-11-12 22:59:30 -0500

    feat: add color

--- pooltool/objects/table/render.py
+++ pooltool/objects/table/render.py
@@ -1,5 +1,7 @@
 import numpy as np
-from panda3d.core import CollisionNode, CollisionPlane, LineSegs, Plane, Point3, Vec3
+from panda3d.core import (CollisionNode, CollisionPlane, Geom, GeomNode,
+                          GeomTriangles, GeomVertexData, GeomVertexFormat,
+                          GeomVertexWriter, LineSegs, Plane, Point3, Vec3)
 
 from pooltool.ani.globals import Global
 from pooltool.config import settings
@@ -119,10 +121,12 @@ class TableRender(Render):
 
     def init_pocket(self, pocket_id):
         pocket = self._table.pockets[pocket_id]
-        circle = self.draw_circle(self.pocket_drawer, pocket.center, pocket.radius, 100)
+        circle = self.draw_circle(
+            self.pocket_drawer, pocket.center, pocket.radius, 100)
         node = Global.render.find("scene").find("table").attachNewNode(circle)
         node.set_shader_auto(True)
         self.nodes[f"pocket_{pocket_id}"] = node
+        self._attach_pocket_marker(pocket_id, pocket)
 
     def init_pockets(self):
         for pocket_id in self._table.pockets:
@@ -134,24 +138,24 @@ class TableRender(Render):
         # draw table as rectangle
         self.init_table()
 
-        if (
+        # Always draw overlays so real tables get visual aids too.
+        draw_wireframe = (
             not self._table.model_descr
             or self._table.model_descr == TableModelDescr.null()
             or not settings.graphics.table
-            or self._table.model_descr.name == TableName.SNOOKER_GENERIC  # dim are WIP
-        ):
-            # draw cushion_segments as edges
-            self.cushion_drawer = LineSegs()
-            self.cushion_drawer.setThickness(3)
-            self.cushion_drawer.setColor(1, 1, 1)
+            or self._table.model_descr.name == TableName.SNOOKER_GENERIC
+        )
+        thickness = 3 if draw_wireframe else 2
 
-            self.init_cushion_edges()
+        self.cushion_drawer = LineSegs()
+        self.cushion_drawer.setThickness(thickness)
+        self.cushion_drawer.setColor(*CUSHION_COLOR)
+        self.init_cushion_edges()
 
-            # draw pockets as unfilled circles
-            self.pocket_drawer = LineSegs()
-            self.pocket_drawer.setThickness(3)
-            self.pocket_drawer.setColor(1, 1, 1)
-            self.init_pockets()
+        self.pocket_drawer = LineSegs()
+        self.pocket_drawer.setThickness(thickness)
+        self.pocket_drawer.setColor(*POCKET_OUTLINE_COLOR)
+        self.init_pockets()
 
         self.init_collisions()
 
@@ -173,7 +177,8 @@ class TableRender(Render):
         return drawer.create()
 
     def get_render_state(self):
-        raise NotImplementedError("Can't call get_render_state for class 'TableRender'")
+        raise NotImplementedError(
+            "Can't call get_render_state for class 'TableRender'")
 
     def set_object_state_as_render_state(self):
         raise NotImplementedError(
@@ -184,3 +189,83 @@ class TableRender(Render):
         raise NotImplementedError(
             "Can't call set_render_state_as_object_state for class 'TableRender'"
         )
+
+    def _attach_pocket_marker(self, pocket_id: str, pocket):
+        """Create a filled marker near the specified pocket."""
+        color = POCKET_MARKER_COLORS.get(pocket_id, DEFAULT_MARKER_COLOR)
+        table_center = np.array([self._table.w / 2, self._table.l / 2])
+        center_xy = np.array([pocket.center[0], pocket.center[1]])
+        direction = center_xy - table_center
+        norm = np.linalg.norm(direction)
+        if norm == 0:
+            direction = np.array([0.0, 1.0])
+        else:
+            direction = direction / norm
+
+        inset = pocket.radius * MARKER_INSET_SCALE
+        marker_center = np.array(
+            [
+                pocket.center[0] - direction[0] * inset,
+                pocket.center[1] - direction[1] * inset,
+                pocket.center[2] + MARKER_Z_OFFSET,
+            ]
+        )
+        marker_radius = pocket.radius * MARKER_BASE_RADIUS_SCALE
+        node = self.nodes["table"].attachNewNode(
+            self._create_filled_circle(marker_center, marker_radius, color)
+        )
+        node.setShaderAuto()
+        self.nodes[f"pocket_marker_{pocket_id}"] = node
+
+    def _create_filled_circle(
+        self,
+        center: np.ndarray,
+        radius: float,
+        color: tuple[float, float, float, float],
+        segments: int = 48,
+    ) -> GeomNode:
+        """Generate a filled 2D circle mesh."""
+        vdata = GeomVertexData(
+            "pocket_marker", GeomVertexFormat.getV3c4(), Geom.UHStatic
+        )
+        vertex = GeomVertexWriter(vdata, "vertex")
+        colors = GeomVertexWriter(vdata, "color")
+
+        vertex.addData3(center[0], center[1], center[2])
+        colors.addData4f(*color)
+
+        for i in range(segments + 1):
+            theta = (2 * np.pi * i) / segments
+            x = center[0] + radius * np.cos(theta)
+            y = center[1] + radius * np.sin(theta)
+            vertex.addData3(x, y, center[2])
+            colors.addData4f(*color)
+
+        triangles = GeomTriangles(Geom.UHStatic)
+        for i in range(1, segments):
+            triangles.addVertices(0, i, i + 1)
+        triangles.closePrimitive()
+
+        geom = Geom(vdata)
+        geom.addPrimitive(triangles)
+
+        node = GeomNode(f"pocket_marker_geom")
+        node.addGeom(geom)
+        return node
+
+
+MARKER_BASE_RADIUS_SCALE = 0.55
+MARKER_INSET_SCALE = -2
+MARKER_Z_OFFSET = 0.06
+POCKET_MARKER_COLORS = {
+    "lb": (1.00, 0.00, 0.00, 1.0),  # Red
+    "lc": (1.00, 0.40, 0.00, 1.0),  # Orange
+    "lt": (0.75, 0.75, 0.75, 1.0),  # Grey
+    "rb": (0.00, 1.00, 0.25, 1.0),  # Green
+    "rc": (0.00, 0.25, 1.00, 1.0),  # Blue
+    "rt": (0.75, 0.00, 1.00, 1.0),  # Purple
+}
+
+DEFAULT_MARKER_COLOR = (0.95, 0.6, 0.2, 1.0)
+CUSHION_COLOR = (0.15, 0.85, 0.35, 1.0)
+POCKET_OUTLINE_COLOR = (0.9, 0.4, 0.2, 1.0)
